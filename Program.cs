68856efde2f4f0cb30ad8a78068a19c4f7e18765
UBDJ <icek314@gmail.com>
2023-07-25 13:57:11 +0900

    20230725

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UB20230725
+{
+
+
+    
+/*    //엔진개발자
+    interface IComponent
+    {
+        void Start();
+
+        void Update();
+    }
+    //head
+
+    class MoveComponent : IComponent
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+
+
+    class JumpComponent : IComponent
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+ 
+
+    }
+
+    class AttackComponent : IComponent
+    {
+        public void Start()
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class GameObject
+    {
+        public List<IComponent> components = new List<IComponent> ();
+    }*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*GameObject gameObject = new GameObject();
+
+            gameObject.components.Add(new MoveComponent());
+            gameObject.components.Add(new JumpComponent());
+            gameObject.components.Add(new AttackComponent());
+
+            gameObject.components[0].Update();
+            gameObject.components[1].Update();
+            gameObject.components[2].Update();
+
+            (gameObject.components[0] as MoveComponent).Move();
+            (gameObject.components[1] as JumpComponent).Jump();*/
+
+
+/*
+            try
+            {
+                int a = 5;
+                int b = 0;
+                float c = a / b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine(`);
+            }*/
+
+
+
+
+        }
+    }
+}
